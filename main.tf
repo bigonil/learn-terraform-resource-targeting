04316d7cb8f69ac64e6502e0bd7f66ea8701c55d
bigonil <luca.bigoni@gmail.com>
2024-10-29 21:28:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,21 +12,20 @@ provider "aws" {
 }
 
 resource "random_pet" "bucket_name" {
-  length    = 3
+  length    = 5
   separator = "-"
   prefix    = "learning"
 }
 
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.1.1"
+  version = ">= 4.1.1"
 
   bucket = random_pet.bucket_name.id
   acl    = "private"
 
   control_object_ownership = true
   object_ownership         = "BucketOwnerPreferred"
-
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
@@ -38,13 +37,12 @@ resource "random_pet" "object_names" {
 
   length    = 5
   separator = "_"
-  prefix    = "learning"
+ # prefix    = "learning"
 }
 
 resource "aws_s3_object" "objects" {
   count = 4
 
-  acl          = "public-read"
   key          = "${random_pet.object_names[count.index].id}.txt"
   bucket       = module.s3_bucket.s3_bucket_id
   content      = "Example object #${count.index}"
--- terraform.tf
+++ terraform.tf
@@ -25,4 +25,3 @@ terraform {
 
   required_version = "~> 1.2"
 }
-
